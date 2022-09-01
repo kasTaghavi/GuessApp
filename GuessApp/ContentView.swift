f33afa3cb088973ac179a4c8eb5e72fb18f9c25a
kasTaghavi <ktaghavi@smu.edu>
2022-09-01 09:59:04 -0500

    Initial Commit

---
+++ GuessApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessApp
+//
+//  Created by Kas Taghavi on 9/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessApp/GuessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessAppApp.swift
+//  GuessApp
+//
+//  Created by Kas Taghavi on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
